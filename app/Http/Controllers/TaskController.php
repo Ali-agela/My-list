b6de1bb997b26a97fdcd8cf8909f314a2bcbbf14
ali agela <aliagela09@gmail.com>
2024-08-12 18:35:04 +0200

    day two tasks completed

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -28,16 +28,42 @@ class TaskController extends Controller
 
     public function index(Request $request)
     {
-        if ($request->has('priority')){
-            $tasks = Task::where('priority','=',$request->input('priority'))->get();
-            return  response()->json([
-                'data' => $tasks
-            ]);
-        }
-        $tasks = Task::all();
+        $tasks = Task::query();
 
+        if ($request->has('priority')){
+            $tasks = $tasks->where('priority','=',$request->input('priority'));
+        }
+        if ($request->has('upcoming')){
+            $tasks = $tasks->where('before_date','>=',date('Y-m-d H-i'));
+        }
+        if ($request->has('sort')){
+            $tasks = $tasks->OrderBy($request->input('sort'));
+        }
+        if($request->has('onlyDone')){
+            $tasks = $tasks->where("is_done","=",true);
+        }
         return response()->json([
-            'data' => $tasks
+            'data' => $tasks->get()
         ]);
+    }
+    public function show( $id){
+        $task = Task::findOrFail($id);
+        return response()->json(['data'=>$task]);
+    }
+    public function update(Request $request, $id){
+    $inputs = $request->validate([
+        'name' => ['string', 'required'],
+            'description' => ['max:1000'],
+            'is_done' => ['boolean'],
+            'before_date' => [],
+            'priority' => ['required', 'string', 'in:low,mid,high']
+        ]);
+        Task::findOrFail($id)->update($inputs);
+        return response()->json(['data'=>'updated task'] );
+    }
+    public function destroy($id){
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(['data'=>'task deleted']) ;
     }
 }
